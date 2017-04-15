36b0e4558ae74c2597f79b1517276b2d6d95e366
hiram <hiram>
2017-04-15 11:54:20 +0800

    net for clamd

---
+++ clamsocket/clamsocket/NetClamd.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace clamsocket
+{
+    class NetClamd
+    {
+        private string _ip;
+
+        private int _port;
+
+        public NetClamd(string ip, int port)
+        {
+            _ip = ip;
+            _port = port;
+        }
+
+        /// <summary>
+        /// 发送命令并执行得到结果
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="needResult"></param>
+        /// <returns></returns>
+        public List<string> SendCmdAndGetRes(string cmd, bool needResult = true)
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                List<string> list = new List<string>();
+                IPAddress ipaddress = IPAddress.Parse(_ip);
+                socket.Connect(new IPEndPoint(ipaddress, _port));
+                socket.Send(Encoding.ASCII.GetBytes(string.Format("n{0}\n", cmd)));
+                if (needResult)
+                {
+                    string result = "...";
+                    while (!string.IsNullOrEmpty(result))
+                    {
+                        byte[] dataArr = new byte[10240];
+                        int len = 0;
+                        for (int i = 0; i < 5; i++)
+                        {
+                            try
+                            {
+                                len = socket.Receive(dataArr);
+                                if (len > 0) break;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("SendCmdAndGetRes异常:{0}", ex.Message);
+                                Thread.Sleep(1000);
+                            }
+                        }
+                        result = Encoding.ASCII.GetString(dataArr, 0, len);
+                        if (!string.IsNullOrEmpty(result)) list.Add(result);
+                    }
+                    return list;
+                }
+            }
+            return null;
+        }
+
+
+        /// <summary>
+        /// 发送命令并执行得到结果
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="needResult"></param>
+        /// <returns></returns>
+        public string SendSimpleCmdAndGetRes(string cmd, bool needResult = true)
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                IPAddress ipaddress = IPAddress.Parse(_ip);
+                socket.Connect(new IPEndPoint(ipaddress, _port));
+                socket.Send(Encoding.ASCII.GetBytes(string.Format("n{0}\n", cmd)));
+                if (needResult)
+                {
+                    byte[] dataArr = new byte[10240];
+                    int len = 0;
+                    for (int i = 0; i < 5; i++)
+                    {
+                        try
+                        {
+                            len = socket.Receive(dataArr);
+                            if (len > 0) break;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("SendCmdAndGetRes异常:{0}", ex.Message);
+                            Thread.Sleep(100);
+                        }
+                    }
+                    return Encoding.ASCII.GetString(dataArr, 0, len);
+                }
+
+            }
+            return "";
+        }
+    }
+}
---
+++ clamsocket/clamsocket/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clamsocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("--------------开始执行----------------");
+
+            try
+            {
+                NetClamd netClamd = new NetClamd("127.0.0.1", 3310);
+
+                string result = netClamd.SendSimpleCmdAndGetRes("PING");
+                Console.WriteLine(result);
+
+                //result = netClamd.SendSimpleCmdAndGetRes("RELOAD");
+                //Console.WriteLine(result);
+
+                List<string> list = netClamd.SendCmdAndGetRes("MULTISCAN /var/test/");
+                foreach (string str in list)
+                { Console.Write(str); }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("执行出现异常:{0}", ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
